7d09ffecd2ca5c574c3d6e562c2277a8f8536d6b
James Cheng <jylcheng@yahoo.com>
2015-11-27 00:41:06 -0800

    Tabs, not spaces.

--- src/main/java/com/zendesk/maxwell/schema/columndef/ColumnDef.java
+++ src/main/java/com/zendesk/maxwell/schema/columndef/ColumnDef.java
@@ -28,69 +28,69 @@ public abstract class ColumnDef {
 		return build(this.tableName, this.name, this.encoding, this.type.name(), this.pos, this.signed, this.enumValues);
 	}
 
-    public static ColumnDef build(String tableName, String name, String encoding, String typeString, int pos, boolean signed, String enumValues[]) {
-        ColumnType type;
-        try {
-            String upperTypeString = unalias_type(typeString.toUpperCase());
-            type = ColumnType.valueOf(upperTypeString);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("unsupported column type " + typeString);
-        }
+	public static ColumnDef build(String tableName, String name, String encoding, String typeString, int pos, boolean signed, String enumValues[]) {
+		ColumnType type;
+		try {
+			String upperTypeString = unalias_type(typeString.toUpperCase());
+			type = ColumnType.valueOf(upperTypeString);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("unsupported column type " + typeString);
+		}
 
-        switch(type) {
-        case BOOL:
-        case BOOLEAN:
-            type = ColumnType.TINYINT;
-            // fallthrough
-        case TINYINT:
-        case SMALLINT:
-        case MEDIUMINT:
-        case INT:
-            return new IntColumnDef(tableName, name, type, pos, signed);
-        case BIGINT:
-            return new BigIntColumnDef(tableName, name, type, pos, signed);
-        case TINYTEXT:
-        case TEXT:
-        case MEDIUMTEXT:
-        case LONGTEXT:
-        case VARCHAR:
-        case CHAR:
-            return new StringColumnDef(tableName, name, type, pos, encoding);
-        case TINYBLOB:
-        case BLOB:
-        case MEDIUMBLOB:
-        case LONGBLOB:
-        case BINARY:
-        case VARBINARY:
-            return new StringColumnDef(tableName, name, type, pos, "binary");
-        case REAL:
-        case NUMERIC:
-            type = ColumnType.DOUBLE;
-            // fall through
-        case FLOAT:
-        case DOUBLE:
-            return new FloatColumnDef(tableName, name, type, pos);
-        case DECIMAL:
-            return new DecimalColumnDef(tableName, name, type, pos);
-        case DATE:
-            return new DateColumnDef(tableName, name, type, pos);
-        case DATETIME:
-        case TIMESTAMP:
-            return new DateTimeColumnDef(tableName, name, type, pos);
-        case YEAR:
-            return new YearColumnDef(tableName, name, type, pos);
-        case TIME:
-            return new TimeColumnDef(tableName, name, type, pos);
-        case ENUM:
-            return new EnumColumnDef(tableName, name, type, pos, enumValues);
-        case SET:
-            return new SetColumnDef(tableName, name, type, pos, enumValues);
-        case BIT:
-            return new BitColumnDef(tableName, name, type, pos);
-        }
+		switch(type) {
+		case BOOL:
+		case BOOLEAN:
+			type = ColumnType.TINYINT;
+			// fallthrough
+		case TINYINT:
+		case SMALLINT:
+		case MEDIUMINT:
+		case INT:
+			return new IntColumnDef(tableName, name, type, pos, signed);
+		case BIGINT:
+			return new BigIntColumnDef(tableName, name, type, pos, signed);
+		case TINYTEXT:
+		case TEXT:
+		case MEDIUMTEXT:
+		case LONGTEXT:
+		case VARCHAR:
+		case CHAR:
+			return new StringColumnDef(tableName, name, type, pos, encoding);
+		case TINYBLOB:
+		case BLOB:
+		case MEDIUMBLOB:
+		case LONGBLOB:
+		case BINARY:
+		case VARBINARY:
+			return new StringColumnDef(tableName, name, type, pos, "binary");
+		case REAL:
+		case NUMERIC:
+			type = ColumnType.DOUBLE;
+			// fall through
+		case FLOAT:
+		case DOUBLE:
+			return new FloatColumnDef(tableName, name, type, pos);
+		case DECIMAL:
+			return new DecimalColumnDef(tableName, name, type, pos);
+		case DATE:
+			return new DateColumnDef(tableName, name, type, pos);
+		case DATETIME:
+		case TIMESTAMP:
+			return new DateTimeColumnDef(tableName, name, type, pos);
+		case YEAR:
+			return new YearColumnDef(tableName, name, type, pos);
+		case TIME:
+			return new TimeColumnDef(tableName, name, type, pos);
+		case ENUM:
+			return new EnumColumnDef(tableName, name, type, pos, enumValues);
+		case SET:
+			return new SetColumnDef(tableName, name, type, pos, enumValues);
+		case BIT:
+			return new BitColumnDef(tableName, name, type, pos);
+		}
 
-        throw new IllegalArgumentException("unsupported column type " + typeString);
-    }
+		throw new IllegalArgumentException("unsupported column type " + typeString);
+	}
 
 	static private String unalias_type(String type) {
 		switch(type) {
